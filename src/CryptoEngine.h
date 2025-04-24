fae82c1ab1db57f623c62c0d4ec22453e4fd4ef3
Naioku <adrian.komuda.dev@gmail.com>
2025-04-24 19:32:39 +0200

    Added: Data encryption.

---
+++ src/CryptoEngine.h
@@ -0,0 +1,15 @@
+﻿#ifndef CRYPTOENGINE_H
+#define CRYPTOENGINE_H
+#include <string>
+
+#include "ICryptoEngine.h"
+
+
+class CryptoEngine final : public ICryptoEngine{
+public:
+    std::string encrypt(const std::string& plaintext, const std::string& password) const override;
+    std::string decrypt(const std::string& ciphertext, const std::string& password) const override;
+};
+
+
+#endif //CRYPTOENGINE_H
---
+++ tests/CryptoEngine.cpp
@@ -0,0 +1,26 @@
+﻿#include "CryptoEngine.h"
+
+#include "gtest/gtest.h"
+
+TEST(CryptoEngineTestSuite, Encrypt)
+{
+    CryptoEngine engine;
+    const std::string plaintext = "secret";
+    const std::string key = "supersecure";
+
+    const std::string encrypted = engine.encrypt(plaintext, key);
+
+    EXPECT_NE(encrypted, plaintext);
+}
+
+TEST(CryptoEngineTestSuite, EncryptAndDecrypt)
+{
+    CryptoEngine engine;
+    const std::string plaintext = "secret";
+    const std::string key = "supersecure";
+
+    const std::string encrypted = engine.encrypt(plaintext, key);
+    const std::string decrypted = engine.decrypt(encrypted, key);
+
+    EXPECT_EQ(decrypted, plaintext);
+}
