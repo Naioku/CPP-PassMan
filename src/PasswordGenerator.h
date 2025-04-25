6d7e0d66a0b49657ce749d5318a78649a5bc0f4a
Naioku <adrian.komuda.dev@gmail.com>
2025-04-26 00:19:07 +0200

    Added: Password generator.

---
+++ src/PasswordGenerator.h
@@ -0,0 +1,24 @@
+﻿#ifndef PASSWORDGENERATOR_H
+#define PASSWORDGENERATOR_H
+#include <string>
+
+
+class PasswordGenerator {
+    const std::string LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+    const std::string UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const std::string DIGITS = "0123456789";
+    const std::string SYMBOLS = "!@#$%^&*()-_=+[]{};:,.<>?";
+
+    int passwordSize;
+
+public:
+    explicit PasswordGenerator(const int& passwordSize);
+    std::string generate() const;
+
+private:
+    static char generateRandomChar(const std::string& charset);
+};
+
+
+
+#endif //PASSWORDGENERATOR_H
