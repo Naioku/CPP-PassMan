85225d2b1826e8dc06814ca197d1c209db0e354d
Naioku <adrian.komuda.dev@gmail.com>
2025-04-27 19:45:36 +0200

    Fixed: Storage: If file at the same name existed in the directory before saving, it was not replaced, but data was appended.

--- tests/Storage.cpp
+++ tests/Storage.cpp
@@ -50,3 +50,54 @@ TEST(IntegrationTestSuiteStorage, SaveAndLoad)
 
     std::filesystem::remove(filePath);
 }
+
+TEST(IntegrationTestSuiteStorage, FileOverriding)
+{
+    const std::string filePath = "test_output.dat";
+    const std::string password = "key123";
+
+    const Storage storage{std::make_shared<CryptoEngine>(), filePath};
+
+    if (std::filesystem::exists(filePath))
+    {
+        std::filesystem::remove(filePath);
+    }
+
+    PasswordEntry passwordEntry1{
+        "gmail",
+        "john",
+        "pass",
+        "note"
+    };
+
+    const std::map<std::string, PasswordEntry> savedEntries1 = {
+        {
+            passwordEntry1.name,
+            passwordEntry1
+        }
+    };
+
+    storage.saveToFile(savedEntries1, password);
+    ASSERT_TRUE(std::filesystem::exists(filePath));
+
+    PasswordEntry passwordEntry2{
+        "gmail2",
+        "john2",
+        "pass2",
+        "note2"
+    };
+
+    const std::map<std::string, PasswordEntry> savedEntries2 = {
+        {
+            passwordEntry2.name,
+            passwordEntry2
+        }
+    };
+
+    storage.saveToFile(savedEntries2, password);
+    ASSERT_TRUE(std::filesystem::exists(filePath));
+
+    const std::map<std::string, PasswordEntry> loadedEntries = storage.loadFromFile(password);
+    const PasswordEntry& loadedEntry = loadedEntries.at(passwordEntry2.name);
+    EXPECT_EQ(loadedEntry.name, passwordEntry2.name);
+}
